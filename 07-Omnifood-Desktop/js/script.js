627037b8b3427e7e091dcfd0f5cd7bf52ba35813
darzwaitz <darzwaitz@yahoo.com>
2023-03-06 23:06:33 +0000

    adding sticky navigation - add js - intersectionobserver - part 1

--- 07-Omnifood-Desktop/js/script.js
+++ 07-Omnifood-Desktop/js/script.js
@@ -37,3 +37,17 @@ allLinks.forEach(function (link) {
       headerEl.classList.toggle("nav-open");
   });
 });
+
+// sticky nav
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0, // when zero % of targeted element is out of view
+  }
+);
+obs.observe(sectionHeroEl);
